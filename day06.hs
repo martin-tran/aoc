d71ff7515bf746156bb4696b72333d7a166d64ec
Martin Tran <martin.hua.tran@gmail.com>
2017-12-22 15:40:45 -0700

    added day 6 part 1 and 2

--- day06.hs
+++ day06.hs
@@ -1,4 +1,4 @@
-import qualified Data.Set
+import Data.List
 
 distribute :: [Int] -> Int -> Int -> [Int]
 distribute xs 0 i = xs
@@ -8,3 +8,25 @@ distribute xs n i = distribute ((take modded xs) ++
                     (n - 1) moddedplus
   where modded = i `mod` (length xs)
         moddedplus = (i + 1) `mod` (length xs)
+
+indexMax :: [Int] -> Int
+indexMax xs
+  | xs == [] || head xs == maximum xs = 0
+  | otherwise = 1 + indexMax (tail xs)
+
+cycleLength :: [Int] -> [[Int]]-> (Int, [[Int]])
+cycleLength xs vec
+  | findIndex (\x -> x == xs) vec /= Nothing = (0, vec ++ [xs])
+  | otherwise = (1 + (fst ans), snd ans)
+    where i = (indexMax xs)
+          iplus = if 1 + i == length xs then 0 else 1 + i
+          xss = (take i xs) ++ [0] ++ (drop (1 + i) xs)
+          ans = cycleLength (distribute xss (maximum xs) iplus) (vec ++ [xs])
+
+main = do
+  contents <- getContents
+  let
+    ans = cycleLength [read x :: Int | x <- words contents] []
+  putStrLn $ "Part 1: " ++ (show $ fst $ ans)
+  putStrLn $ "Part 2: " ++
+    (show $ maybe 0 (\x -> (fst ans) - x) (findIndex (\x -> x == last (snd ans)) (snd ans)))
